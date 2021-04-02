3f4d03e3378ca2a2eda7fd88d485fef4e279cc3b
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2021-04-02 15:50:46 +0600

    Check repo deeply

--- push.bash
+++ push.bash
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-
-if [[ $(git diff --stat) != '' ]]; then
+if [ -z "$(git status --porcelain)" ]; then
+	echo 'Repo is clean'
+else
 	if [[ "$1" != '' ]]; then
 		git config user.name jetcodex
 	    git config user.email jetbot@jetcodex.xyz
 	    git add .
 	    git commit -m "$1"
 	    git push -u origin main
-	fi
-else
-    echo 'Repo is clean'
+	fi    
 fi
