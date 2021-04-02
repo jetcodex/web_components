9946545835b5391e252099f53c15074c4657176c
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2021-04-02 15:15:35 +0600

    Add argument to assign commit message

--- push.bash
+++ push.bash
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ $(git diff --stat) != '' ]]; then
-    # git config user.name jetcodex
-    # git config user.email jetbot@jetcodex.xyz
-    git add .
-    git commit -m "$1"
-    git push -u origin main
+	if [[ "$1" != '' ]]; then
+		git config user.name jetcodex
+	    git config user.email jetbot@jetcodex.xyz
+	    git add .
+	    git commit -m "$1"
+	    git push -u origin main
+	fi
 else
     echo 'Repo is clean'
 fi
